9bb1cf6b347eaaf2c812163c20acaf0d7423a316
Nicat Alioglu <142000034+NicatAli@users.noreply.github.com>
2024-09-14 15:24:55 +0400

    Create task.cs

---
+++ task.cs
@@ -0,0 +1,65 @@
+﻿int birthday = 2026;
+
+if (birthday < 2026)
+{
+    Console.WriteLine("18 yashiniz tamam olmayib!");
+}
+else
+{
+    Console.WriteLine("18 yashiniz tamam olub!");
+}
+
+
+
+
+
+
+int num = 5;
+
+if (num < 0)
+{
+    Console.WriteLine("Eded menfidir.");
+}
+else
+    Console.WriteLine("Eded musbetdir.");
+
+
+
+
+
+
+int day = 7;
+switch (day) 
+{
+case 1:
+        Console.WriteLine("Bazar Ertesi");
+        break;
+    case 2:
+        Console.WriteLine("Chershenbe Axshami");
+        break;
+    case 3:
+        Console.WriteLine("Chershenbe");
+        break;
+        case 4:
+        Console.WriteLine("Cume Axshami");
+        break;
+        case 5:
+        Console.WriteLine("Cume");
+        break;
+        case 6:
+        Console.WriteLine("Shenbe");
+        break;
+    case 7:
+        Console.WriteLine("Bazar");
+            break;
+
+}
+
+
+
+string[] monthsofYear = { "Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avqust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr" };
+Console.WriteLine(monthsofYear[5]);
+
+//iyun gorunecek.
+
+

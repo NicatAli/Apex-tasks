3477f5c0c5d14d9d5660dda0da28ff0cb548283e
Nicat Alioglu <142000034+NicatAli@users.noreply.github.com>
2024-10-16 12:53:32 +0400

    task

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Models.Base
+{
+    public class Animal
+    {
+       public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Animal is sleeping");
+        }
+
+    }
+}
---
+++ Character.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp12;
+
+static void Main(string[] args)
+{
+    
+    Warrior warrior = new Warrior("Warrior", 50, 15);
+    Mage mage = new Mage("Mage", 40, 12);
+    Archer archer = new Archer("Archer", 35, 10);
+
+    
+    warrior.Attack(mage);  
+    Console.WriteLine();
+
+    mage.Attack(archer);   
+    Console.WriteLine();
+
+    archer.Attack(warrior); 
+    Console.WriteLine();
+}

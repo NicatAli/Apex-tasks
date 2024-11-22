e6ae1a9c1612d9385145eb7f2551ae6dab5cee4a
Nicat Alioglu <142000034+NicatAli@users.noreply.github.com>
2024-11-22 11:59:10 +0400

    task

---
+++ task 2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+interface IAccount
+{
+    bool PasswordChecker(string password); 
+    void ShowInfo(); 
+}
+
+class User : IAccount
+{
+    public string Fullname { get; set; }
+    public string Email { get; set; }
+    private string _password;
+
+    public string Password
+    {
+       
+        get { return _password; }
+
+        set
+        {
+            if (PasswordChecker(value)) 
+            {
+                _password = value;
+            }
+            else
+            {
+                throw new Exception("The password does not comply with the specified rules!");
+            }
+        }
+    }
+
+    public bool PasswordChecker(string password)
+    {
+
+        if (password.Length < 8) return false;      
+        if (!Regex.IsMatch(password, "[A-Z]")) return false;
+        if (!Regex.IsMatch(password, "[a-z]")) return false;        
+        if (!Regex.IsMatch(password, "[0-9]")) return false;
+
+        return true; 
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Fullname: {Fullname}");
+        Console.WriteLine($"Email: {Email}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Name and Surname: ");
+            string fullname = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Password: (At least 8 characters.\r\nAt least 1 uppercase letter.\r\nAt least 1 lowercase letter.\r\nAt least 1 digit.) ");
+            string password = Console.ReadLine();
+
+            User user = new User
+            {
+                Fullname = fullname,
+                Email = email,
+                Password = password 
+            };
+
+            Console.WriteLine("\nUser added succesfully!");
+            user.ShowInfo();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nError: {ex.Message}");
+        }
+    }
+}
---
+++ task4.cs
@@ -0,0 +1,41 @@
+﻿class Person
+{
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public bool IsAdult { get; private set; }
+
+  
+    public Person(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        IsAdult = age > 18; 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Surname: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("Enter age: ");
+        int age = int.Parse(Console.ReadLine());
+
+       
+        Person person = new Person(name, surname, age);
+
+       
+        Console.WriteLine($"\nName: {person.Name}");
+        Console.WriteLine($"Surname: {person.Surname}");
+        Console.WriteLine($"Age: {person.Age}");
+        Console.WriteLine($"Is Adult? {(person.IsAdult ? "Yes" : "No")}");
+    }
+}

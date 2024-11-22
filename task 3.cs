baed8e8b381650e540f430b1827466e375652d0a
Nicat Alioglu <142000034+NicatAli@users.noreply.github.com>
2024-11-22 11:47:03 +0400

    Create task 3.cs

---
+++ task 3.cs
@@ -0,0 +1,32 @@
+﻿class Student
+{
+    private static int lastAge = 20;
+    public int Age { get; private set; }
+
+    public Student()
+    {
+        Age = ++lastAge;
+    }
+
+}
+
+class Program
+{ 
+   static void Main(string[] args)
+    {
+        Student tamerlan = new Student();
+        Console.WriteLine(tamerlan.Age);
+
+        Student faiq = new Student();
+        Console.WriteLine(faiq.Age);
+
+        Student tural = new Student();
+        Console.WriteLine(tural.Age);
+
+        Student emil = new Student();
+        Console.WriteLine(emil.Age);
+    }
+
+
+
+}
